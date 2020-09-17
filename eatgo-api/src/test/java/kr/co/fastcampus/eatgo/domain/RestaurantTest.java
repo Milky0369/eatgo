d5fd02659e8d50238b6e46c8a21b7880e68ac088
Milky <tpdhks0369@gmail.com>
2020-09-17 22:29:40 +0900

    feat : TDD를 적용한 개발 방법 연습

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/domain/RestaurantTest.java
@@ -0,0 +1,30 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+
+public class RestaurantTest {
+
+    @Test
+    public void creation() {
+
+        Restaurant restaurant = new Restaurant("Bob zip", "");
+
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("Seoul"));
+
+    }
+
+    @Test
+    public void information() {
+
+        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+
+        assertThat(restaurant.getInfomation(), is("Bob zip in Seoul"));
+
+    }
+
+}
